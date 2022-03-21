b501e25472adc4aa02a0ef3d7c335b203e51db51
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-21 18:26:49 +0100

    commit test event click depends on period

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
-// function to get the data from the json file
-
+// function async to get data from the json file
 async function loadData() {
-  const response = await fetch("./js/data.json");
+  const response = await fetch("./assets/js/data.json");
   const data = await response.json();
   console.log(data);
 }
 loadData();
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Call elements from the DOM
+  const period = document.getElementsByClassName("main__infosHeader");
+
+  document.querySelectorAll(".main__infosHeader").forEach((element) => {
+    element.addEventListener("click", () => {
+      if (element.classList.contains("day")) {
+        console.log("day");
+      } else if (element.classList.contains("week")) {
+        console.log("week");
+      } else if (element.classList.contains("month")) {
+        console.log("month");
+      }
+    });
+  });
+});
