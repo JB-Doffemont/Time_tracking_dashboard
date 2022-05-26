1e07274ca79c7078f6b025d35f2317c2618089c5
JB <jeanbernard_doffemont@yahoo.fr>
2022-05-26 17:10:30 +0200

    commit display data with loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,26 +6,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   // Call elements from the DOM
   const titles = document.querySelectorAll(".title");
-  const studyTitle = document.getElementById("study");
-  const selfcareTitle = document.getElementById("selfcare");
-  const playTitle = document.getElementById("play");
-  const socialTitle = document.getElementById("social");
-  const currentHoursWork = document.getElementById("currentHoursWork");
-  const currentHoursPlay = document.getElementById("currentHoursPlay");
-  const currentHoursSelfcare = document.getElementById("currentHoursSelfcare");
-  const currentHoursSocial = document.getElementById("currentHoursSocial");
-  const currentHoursStudy = document.getElementById("currentHoursStudy");
-  const currentHoursExercise = document.getElementById("currentHoursExercise");
-  const previousHoursWork = document.getElementById("previousHoursWork");
-  const previousHoursPlay = document.getElementById("previousHoursPlay");
-  const previousHoursSelfcare = document.getElementById(
-    "previousHoursSelfcare"
-  );
-  const previousHoursSocial = document.getElementById("previousHoursSocial");
-  const previousHoursStudy = document.getElementById("previousHoursStudy");
-  const previousHoursExercise = document.getElementById(
-    "previousHoursExercise"
-  );
+  const currentHours = document.querySelectorAll(".currentHours");
+  const previousHours = document.querySelectorAll(".previousHours");
 
   function displayTitle() {
     for (let i = 0; i < data.length; i++) {
@@ -58,81 +40,29 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   function loadDayData() {
-    currentHoursWork.innerHTML = `${data[0].timeframes.daily.current}` + "hrs";
-    currentHoursPlay.innerHTML = `${data[1].timeframes.daily.current}` + "hrs";
-    currentHoursStudy.innerHTML = `${data[2].timeframes.daily.current}` + "hrs";
-    currentHoursExercise.innerHTML =
-      `${data[3].timeframes.daily.current}` + "hrs";
-    currentHoursSocial.innerHTML =
-      `${data[4].timeframes.daily.current}` + "hrs";
-    currentHoursSelfcare.innerHTML =
-      `${data[5].timeframes.daily.current}` + "hrs";
+    for (let i = 0; i < data.length; i++) {
+      const { daily } = data[i].timeframes;
 
-    previousHoursWork.innerHTML =
-      "Yesterday - " + `${data[0].timeframes.daily.previous}` + " hrs";
-    previousHoursPlay.innerHTML =
-      "Yesterday - " + `${data[1].timeframes.daily.previous}` + " hrs";
-    previousHoursStudy.innerHTML =
-      "Yesterday - " + `${data[2].timeframes.daily.previous}` + " hrs";
-    previousHoursExercise.innerHTML =
-      "Yesterday - " + `${data[3].timeframes.daily.previous}` + " hrs";
-    previousHoursSocial.innerHTML =
-      "Yesterday - " + `${data[4].timeframes.daily.previous}` + " hrs";
-    previousHoursSelfcare.innerHTML =
-      "Yesterday - " + `${data[5].timeframes.daily.previous}` + " hrs";
+      currentHours[i].innerHTML = `${daily.current}` + "hrs";
+      previousHours[i].innerHTML = `${daily.previous}` + "hrs";
+    }
   }
 
   function loadWeekData() {
-    currentHoursWork.innerHTML = `${data[0].timeframes.weekly.current}` + "hrs";
-    currentHoursPlay.innerHTML = `${data[1].timeframes.weekly.current}` + "hrs";
-    currentHoursStudy.innerHTML =
-      `${data[2].timeframes.weekly.current}` + "hrs";
-    currentHoursExercise.innerHTML =
-      `${data[3].timeframes.weekly.current}` + "hrs";
-    currentHoursSocial.innerHTML =
-      `${data[4].timeframes.weekly.current}` + "hrs";
-    currentHoursSelfcare.innerHTML =
-      `${data[5].timeframes.weekly.current}` + "hrs";
+    for (let i = 0; i < data.length; i++) {
+      const { weekly } = data[i].timeframes;
 
-    previousHoursWork.innerHTML =
-      "Last Week - " + `${data[0].timeframes.weekly.previous}` + " hrs";
-    previousHoursPlay.innerHTML =
-      "Last Week - " + `${data[1].timeframes.weekly.previous}` + " hrs";
-    previousHoursStudy.innerHTML =
-      "Last Week - " + `${data[2].timeframes.weekly.previous}` + " hrs";
-    previousHoursExercise.innerHTML =
-      "Last Week - " + `${data[3].timeframes.weekly.previous}` + " hrs";
-    previousHoursSocial.innerHTML =
-      "Last Week - " + `${data[4].timeframes.weekly.previous}` + " hrs";
-    previousHoursSelfcare.innerHTML =
-      "Last Week - " + `${data[5].timeframes.weekly.previous}` + " hrs";
+      currentHours[i].innerHTML = `${weekly.current}` + "hrs";
+      previousHours[i].innerHTML = `${weekly.previous}` + "hrs";
+    }
   }
 
   function loadMonthData() {
-    currentHoursWork.innerHTML =
-      `${data[0].timeframes.monthly.current}` + "hrs";
-    currentHoursPlay.innerHTML =
-      `${data[1].timeframes.monthly.current}` + "hrs";
-    currentHoursStudy.innerHTML =
-      `${data[2].timeframes.monthly.current}` + "hrs";
-    currentHoursExercise.innerHTML =
-      `${data[3].timeframes.monthly.current}` + "hrs";
-    currentHoursSocial.innerHTML =
-      `${data[4].timeframes.monthly.current}` + "hrs";
-    currentHoursSelfcare.innerHTML =
-      `${data[5].timeframes.monthly.current}` + "hrs";
+    for (let i = 0; i < data.length; i++) {
+      const { monthly } = data[i].timeframes;
 
-    previousHoursWork.innerHTML =
-      "Last Month - " + `${data[0].timeframes.monthly.previous}` + " hrs";
-    previousHoursPlay.innerHTML =
-      "Last Month - " + `${data[1].timeframes.monthly.previous}` + " hrs";
-    previousHoursStudy.innerHTML =
-      "Last Month - " + `${data[2].timeframes.monthly.previous}` + " hrs";
-    previousHoursExercise.innerHTML =
-      "Last Month - " + `${data[3].timeframes.monthly.previous}` + " hrs";
-    previousHoursSocial.innerHTML =
-      "Last Month - " + `${data[4].timeframes.monthly.previous}` + " hrs";
-    previousHoursSelfcare.innerHTML =
-      "Last Month - " + `${data[5].timeframes.monthly.previous}` + " hrs";
+      currentHours[i].innerHTML = `${monthly.current}` + "hrs";
+      previousHours[i].innerHTML = `${monthly.previous}` + "hrs";
+    }
   }
 });
